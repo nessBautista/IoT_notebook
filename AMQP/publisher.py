feda2be95ec0d9b8c8262863af1407f740a2350b
Ness <nestor.jbautista@gmail.com>
2024-03-14 13:12:40 -0600

    Added AMQP publisher and consumer scripts

---
+++ AMQP/publisher.py
@@ -0,0 +1,25 @@
+import pika
+# Este script se encarga de publicar un mensaje a una cola de trabajo de RabbitMQ.
+
+# Establecemos la conexión a RabbitMQ que esta corriendo en nuestro localhost
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost')
+)
+
+# Necesitamos crear una conexión a un canal para poder enviar mensajes
+channel = connection.channel()
+
+# Ahora creamos una cola de trabajo
+channel.queue_declare(queue='IoT_Equipo8')
+
+
+# Publicamos nuestro mensaje
+channel.basic_publish(
+    exchange='',
+    routing_key='IoT_Equipo8',
+    body= 'Hola desde python script!'
+)
+print(" our message was published")
+
+# Finalmente cerramos la conexión
+connection.close()
